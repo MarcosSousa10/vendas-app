4f792d33105c9912a50ad1fe7c32912ed72e8048
marcos sousa <marcospegodesousa10@gmail.com>
2023-01-15 08:14:23 -0300

    15

--- src/components/layout/menu/index.tsx
+++ src/components/layout/menu/index.tsx
@@ -1,7 +1,7 @@
 export const Menu: React.FC=()=>{
     return(
         <aside className="column is-2 is-narrow-mobile s-fullheight section is-hidden-mobile">
-            <p className="menu-label is-hidden-touch"></p>
+            <p className="menu-label is-hidden-touch"/>
                 <ul className="menu-list">
                     <li>
                         <a href="#">
