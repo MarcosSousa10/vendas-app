b78cd6f3e81105e059da9282ef0bc754dfce922e
marcos sousa <marcospegodesousa10@gmail.com>
2023-01-22 17:38:30 -0300

    220123

--- src/components/clientes/cadastro/form.tsx
+++ src/components/clientes/cadastro/form.tsx
@@ -1,32 +1,65 @@
 import { Cliente } from "../../../app/models/clientes";
 import { useFormik } from "formik";
-
-interface ClienteFormProps{
-    cliente:Cliente;
-    onSubmit:(cliente:Cliente)=>void;
+import { Input } from "../../common/input";
+import { formatReal } from "../../../app/util/money";
+interface ClienteFormProps {
+    cliente: Cliente;
+    onSubmit: (cliente: Cliente) => void;
 }
-const formScheme:Cliente={
-    cadastro:'',
-    cpf:'',
-    dataNascimento:'',
-    email:'',
-    id:'',
-    nome:'',
-    telefone:''
+const formScheme: Cliente = {
+    cadastro: '',
+    cpf: '',
+    dataNascimento: '',
+    email: '',
+    id: '',
+    nome: '',
+    telefone: ''
 }
-export const ClienteForm:React.FC<ClienteFormProps>=({
+export const ClienteForm: React.FC<ClienteFormProps> = ({
     cliente,
     onSubmit
-})=>{
-    const formik=useFormik<Cliente>({
-        initialValues:{...cliente, ...formScheme},
+}) => {
+    const formik = useFormik<Cliente>({
+        initialValues: { ...formScheme,...cliente},
         onSubmit,
     });
-    return(
+    return (
         <form onSubmit={formik.handleSubmit}>
-            <input value={formik.values.nome} onChange={formik.handleChange}
-            id="nome" name="nome"/>
-            <button type="submit">Enviar</button>
+            {formik.values.id &&
+                <div className="columns">
+
+                    <Input id="id" name="id" disabled
+                        value={formik.values.id} label="Codigo:" columnClasses="is-half" autoComplete="off" />
+                    <Input id="cadastro" name="cadastro" columnClasses="is-half" disabled
+                        value={formik.values.cadastro} label="Data Cadastro " autoComplete="off" />
+                </div>}
+            <div className="columns">
+                <Input id="nome" name="nome" onChange={formik.handleChange}
+                    value={formik.values.nome} label="Nome *" columnClasses="is-full" autoComplete="off" />
+            </div>
+            <div className="columns">
+                <Input id="cpf" name="cpf" onChange={formik.handleChange}
+                    value={formik.values.cpf} label="CPF *" columnClasses="is-half" autoComplete="off" />
+                <Input id="dataNascimento" name="dataNascimento" columnClasses="is-half" onChange={formik.handleChange}
+                    value={formik.values.dataNascimento} label="Data Nascimento *" autoComplete="off" />
+            </div>
+            <div className="columns">
+                <Input id="endereco" name="endereco" columnClasses="is-full" onChange={formik.handleChange}
+                    value={formik.values.endereco} label="Endereço *" autoComplete="off" />
+            </div>
+            <div className="columns">
+                <Input id="email" name="email" onChange={formik.handleChange}
+                    value={formik.values.email} label="Email *" columnClasses="is-half" autoComplete="off" />
+                <Input id="telefone" name="telefone" columnClasses="is-half" onChange={formik.handleChange}
+                    value={formik.values.telefone} label="Telefone *" autoComplete="off" />
+            </div>
+            <div className="field is-grouped">
+                <button type="submit" className="button" >
+                    {formik.values.id ? "Atualizar" : "Salvar"}
+                </button>
+            </div>
+
+
         </form>
     )
 }
--- src/components/clientes/cadastro/index.tsx
+++ src/components/clientes/cadastro/index.tsx
@@ -2,15 +2,15 @@ import Layout from "../../layout";
 import { useState } from "react";
 import { Cliente } from "../../../app/models/clientes";
 import { ClienteForm } from "./form";
-export const  CadastroCliente:React.FC=()=>{
-    const [cliente,setCliente]= useState<Cliente>({});
-   const handleSubmit=(cliente:Cliente)=>{
+export const CadastroCliente: React.FC = () => {
+    const [cliente, setCliente] = useState<Cliente>({});
+    const handleSubmit = (cliente: Cliente) => {
         console.log(cliente);
     }
-    return(
+    return (
         <Layout titulo="Clientes">
-            <ClienteForm cliente={cliente} onSubmit={handleSubmit}/>
+            <ClienteForm cliente={cliente} onSubmit={handleSubmit} />
         </Layout>
-        
+
     )
 }
