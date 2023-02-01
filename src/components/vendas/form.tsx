e09174acbb36ef430e0f7992fe913ba1524185fc
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-02-01 10:48:46 -0300

    010202023

--- src/components/vendas/form.tsx
+++ src/components/vendas/form.tsx
@@ -1,31 +1,89 @@
 import { Venda } from "../../app/models/vendas";
 import { useFormik } from "formik";
-import {AutoComplete} from 'primereact/autocomplete';
-interface VendasFormProps{
-    onSubmit:(venda :Venda)=> void;
+import {
+    AutoComplete,
+    AutoCompleteChangeParams,
+    AutoCompleteCompleteMethodParams
+} from 'primereact/autocomplete';
+import { useState } from "react";
+import { Page } from "../../app/models/common";
+import { Cliente } from "../../app/models/clientes";
+import { useClienteService } from "../../app/services/cliente.service";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+interface VendasFormProps {
+    onSubmit: (venda: Venda) => void;
 }
 const formScheme: Venda = {
-    cliente:{},
-    produtos:[],
-    total:0,
-    formaPagamento:''
+    cliente: null,
+    produtos: [],
+    total: 0,
+    formaPagamento: ''
 }
-export const VendasForm:React.FC<VendasFormProps> = ({
+export const VendasForm: React.FC<VendasFormProps> = ({
     onSubmit,
-})=>{
-    const formik= useFormik<Venda>({
+}) => {
+    const clienteService = useClienteService();
+    const [codigoProduto, setCodigoProduto] = useState<string>('');
+    const [listaClientes, setListaClientes] = useState<Page<Cliente>>({
+        content: [],
+        first: 0,
+        size: 0,
+        totalElements: 0
+    })
+    const formik = useFormik<Venda>({
         onSubmit,
         initialValues: formScheme
     })
-    return(
+    const handleClienteAutoComplete = (e: AutoCompleteCompleteMethodParams) => {
+        const nome = e.query;
+        clienteService.find(nome, '', 0, 20)
+            .then(cliente => setListaClientes(cliente))
+    }
+    const handleClienteChange = (e: AutoCompleteChangeParams) => {
+        const clienteSelecionado: Cliente = e.value;
+        formik.setFieldValue("cliente", clienteSelecionado)
+    }
+    const handleCodigoProdutoSelect = (event: any) => {
+        console.log(codigoProduto)
+    }
+    return (
         <form onSubmit={formik.handleSubmit}>
             <div className="p-fluid">
                 <div className="p-field">
                     <label htmlFor="cliente">Cliente: *</label>
-                    <AutoComplete id="cliente" name="cliente"/>  
+                    <AutoComplete value={formik.values.cliente}
+                        onChange={handleClienteChange} field="nome" suggestions={listaClientes.content}
+                        completeMethod={handleClienteAutoComplete} id="cliente" name="cliente" />
                 </div>
+                <div className="p-grid">
+                    <div className="p-col-2">
+                        <span className="p-float-label">
+                            <InputText onBlur={handleCodigoProdutoSelect} id="codigoProduto" value={codigoProduto} onChange={e => setCodigoProduto(e.target.value)} />
+                            <label htmlFor="codigoProduto">Codigo</label>
+                        </span>
+                    </div>
+                    <div className="p-col-6">
+                    <span className="p-float-label">
+                            <AutoComplete />
+                        </span>
+                    </div>
+                    <div className="p-col-2">
+                    <span className="p-float-label">
+                            <InputText id="qtdProduto" value={codigoProduto} />
+                            <label htmlFor="qtdProduto">QTD</label>
+                        </span>
+                    </div>
+                    <div className="p-col-2">
+                        <Button label="adicionar"/>
+                    </div>
+                    </div>
+                    
+                    
+
+
+                <Button type="submit" label="Finalizar" />
             </div>
-            
         </form>
     )
 }
