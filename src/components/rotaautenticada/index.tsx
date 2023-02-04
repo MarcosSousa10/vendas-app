72137edae00c9c9f07b9e3c1c715c4e9a79d1390
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-02-04 16:25:49 -0300

    04022023

--- src/components/rotaautenticada/index.tsx
+++ src/components/rotaautenticada/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import { Loader } from "../common/loader";
 
 interface RotaAutenticadaProps{
     children: React.ReactNode;
@@ -7,9 +8,10 @@ export const RotaAutenticada:React.FC<RotaAutenticadaProps>=({
     children
 })=>{
   const { data: session } = useSession()
+
   if (session) {
     return (
-        <>
+       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
         {children}
@@ -23,3 +25,5 @@ export const RotaAutenticada:React.FC<RotaAutenticadaProps>=({
     </>
   )
 }
+
+
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,16 @@ import '../components/common/loader/loader.css'
 import 'primereact/resources/primereact.min.css'
 import 'primereact/resources/themes/md-light-indigo/theme.css'
 import 'primeflex/primeflex.css'
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { SessionProvider } from "next-auth/react"
+import { RotaAutenticada } from '../components/rotaautenticada'
+
+export default function App({ Component,  pageProps: { session, ...pageProps }, }: AppProps) {
+  return (
+<SessionProvider session={pageProps.session}>
+  <RotaAutenticada >
+    <Component {...pageProps} />
+  </RotaAutenticada>
+    
+    </SessionProvider>
+  )
 }
