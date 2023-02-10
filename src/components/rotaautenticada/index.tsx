fec79011d90d521223551dd56cb157b22664b7c5
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-02-10 13:34:35 -0300

    login

--- src/components/rotaautenticada/index.tsx
+++ src/components/rotaautenticada/index.tsx
@@ -25,27 +25,25 @@ export const RotaAutenticada: React.FC<RotaAutenticadaProps> = ({
   }
   return (
     <div className="container" >
-      <Card style={{ minWidth: '100%', minHeight: '100vh', display:"flex" }}>
-        <div style={{ alignItems: "center",textAlign:"center" }}>
+      <Card style={{ minWidth: '100%', minHeight: '100vh', display: "flex" }}>
+        <div style={{ alignItems: "center", textAlign: "center" }}>
           <Card.Img variant="top" src="https://ps.w.org/login-customizer/assets/icon-256x256.png?rev=2455454" style={{ width: '400px', }} ></Card.Img>
-          <Card.Title><h5 style={{fontWeight: 'bold', fontSize:"50px"}}>Login</h5></Card.Title>
+          <Card.Title><h5 style={{ fontWeight: 'bold', fontSize: "50px", fontFamily: "sans-serif" }}>Login</h5></Card.Title>
         </div>
 
         <div className="row">
-          <div className="col-6" >
-            <form action="" className="m-5" style={{ display:"block",  alignItems: "center", justifyContent: "center"}}>
+          <div className="col" >
+            <form action="" className="m-5" style={{ display: "block", alignItems: "center", justifyContent: "center" }}>
               <InputCPF id="cpf" name="cpf" label="CPF" className='form-control' />
               <InputEmail id="email" name="email" label="Email" className='form-control' />
               <Input id="nome" name="nome" label="Nome" className='form-control' />
               <Input type="password" id="senha" name="senha" label="Senha" className='form-control' />
               <InputTelefone id="telefone" name="telefone" label="Telefone" className='form-control' />
-              <br />
-              <br />
-              <div style={{textAlign:"center" }}><Button >Cadastrar</Button></div>
-              
+              <div style={{ textAlign: "center" }} className="m-5"><Button >Cadastrar</Button></div>
+
             </form>
           </div>
-          <div className="col-6">
+          <div className="col">
             <div className="car m-5">
 
 
@@ -53,25 +51,38 @@ export const RotaAutenticada: React.FC<RotaAutenticadaProps> = ({
 
               <Card.Body >
 
-                
-                <Card.Text>
+
+                <Card.Text style={{ textAlign: "center", fontFamily: 'fantasy' }}>
                   Faça o Login ultilizando Uma Das Redes Sociais
-                  <br />
                   <br />
 
                 </Card.Text>
-                <Button className="button is-large" onClick={() => signIn()}>  <span className="icon is-medium">
-                  <Icon path={mdiAccount}
-                    title="User Profile"
-                    size={1}
-                    horizontal
-                    vertical
-                    rotate={90}
-                    color="red"
-                    spin
-                  />
-                </span>
-                  <span>Entrar Ultilizando Redes Sociais</span></Button>
+
+
+                <InputEmail id="email" name="email" label="Email" className='form-control' />
+                <Input type="password" id="senha" name="senha" label="Senha" className='form-control' />
+                <div className="container">
+                  <div className="row mt-5">
+                    <div className="col">
+                      <div style={{ textAlign: "center" }} ><Button className="button is-large" onClick={() => signIn()}>  <span className="icon is-medium">
+                        <Icon path={mdiAccount}
+                          title="User Profile"
+                          size={1}
+                          horizontal
+                          vertical
+                          rotate={90}
+                          color="red"
+                          spin
+                        />
+                      </span>
+                        <span>Entrar Ultilizando Redes Sociais</span></Button></div>
+                    </div>
+
+                    <div className="col">
+                      <div style={{ textAlign: "center" }} ><Button > <span>Login</span> </Button></div>
+                    </div>
+                  </div>
+                </div>
               </Card.Body>
 
             </div>
