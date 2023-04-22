169bd03c444c92a6cb39f3a5a909df578db521df
MarcosSousa10 <marcospegodesosua10@gmail.com>
2023-04-22 15:55:38 -0300

    heroko

--- src/app/http/index.ts
+++ src/app/http/index.ts
@@ -1,4 +1,4 @@
 import axios, {AxiosInstance} from "axios";
 export const httpClient:AxiosInstance = axios.create({
-    baseURL:"http://192.168.1.21:8080"
+    baseURL:"https://supremogas.herokuapp.com/"
 })
