02f80bd16f1190b8e9dece8b353e8f352e66145b
MarcosSousa10 <marcospegodesosua10@gmail.com>
2023-04-23 08:44:56 -0300

    f

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,13 +1,18 @@
 import NextAuth from "next-auth"
- import Providers from "next-auth/providers/github"
+
+ import GitHubProvider from "next-auth/providers/github";
 export const authOptions = {
 
 }
 export default NextAuth({
-     providers:[
-         Providers({
-             clientId: process.env.NEXT_PUBLIC_GITHUB_PROVIDER_CLIENTID,
-             clientSecret: process.env.NEXT_PUBLIC_GITHUB_PROVIDER_CLIENTSECRET         
-            }),
-  ],}
+  
+     
+   
+      providers: [
+        GitHubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET
+        })
+      ]
+    }
  )
