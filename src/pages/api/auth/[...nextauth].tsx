676187ffd3307efa65990c95a41ecfed5416d30c
MarcosSousa10 <marcospegodesosua10@gmail.com>
2023-04-23 08:30:23 -0300

    25

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -10,9 +10,9 @@ export const authOptions = {
 export default NextAuth({
      providers:[
          Providers({
-             clientId: 'Iv1.c9096e133ca5352d',
-             clientSecret: '5d653fc8a6b8770da117c441daac3a01386988bb',
-         }),
+             clientId: process.env.NEXT_PUBLIC_GITHUB_PROVIDER_CLIENTID,
+             clientSecret: process.env.NEXT_PUBLIC_GITHUB_PROVIDER_CLIENTSECRET         
+            }),
          Auth0Provider({
          clientId: 'x834JoVESoE8G75knKIrgC28sHse7Fvv',
            clientSecret: 'VsvICoKeWehfxluTvQjR_8_6pcph6ycajBZnrFwvlcOR4juQXgFhEwtKNtzpZHVV',
