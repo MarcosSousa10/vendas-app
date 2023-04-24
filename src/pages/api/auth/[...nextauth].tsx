6d70d2c355c970dd047588e652ab46b215c212b6
Marcos <marcospegodesousa10@gmail.com>
2023-04-24 11:17:42 -0300

    d

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-
+import CredentialsProvider from "next-auth/providers/credentials"
  import GitHubProvider from "next-auth/providers/github";
 export const authOptions = {
 
@@ -13,7 +13,20 @@ export default NextAuth({
           clientId: process.env.GITHUB_ID ?? "",
           clientSecret: process.env.GITHUB_SECRET ?? ""
         }),
-      ],   
+        CredentialsProvider({
+          name:"NextAuthCredentials",
+          credentials:{},
+           async authorize(credentials) {
+               console.log(credentials)
+               return{
+                   id: "1",
+                   name:"marcos",
+                   email:"marcospegodesousa10@gmail.com",
+                   image:"gsds"
+               };
+              },
+            })
+          ],
       secret: 'fadfadasdpiasjdsad524ad534a45d34a35d4a65d4a56s4da54d6a84sd658a4sd6584asd',
     }
  )
