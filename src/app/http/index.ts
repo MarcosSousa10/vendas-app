698109508d0b5029fcb465d0b63ec855d0c615bc
Marcos <marcospegodesousa10@gmail.com>
2023-04-28 10:18:19 -0300

    d

--- src/app/http/index.ts
+++ src/app/http/index.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosInstance} from "axios";
 export const httpClient:AxiosInstance = axios.create({
-    //  baseURL:process.env.NEXT_PUBLIC_VENDAS_API
-    baseURL:'http://192.168.2.181:8080'
+    baseURL:process.env.NEXT_PUBLIC_VENDAS_API
+    // baseURL:'http://192.168.2.181:8080'
 })
